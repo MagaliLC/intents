1246b4ae9ad0a059f792f71e80a758de20a2a44f
Magalí Lescano Correa <magalilescano@gmail.com>
2019-06-12 12:56:45 +0200

    Contador de veces en las que se entra en la Second Activity

--- Intents_Exercise/app/src/main/java/com/example/intents_exercise/MainActivity.java
+++ Intents_Exercise/app/src/main/java/com/example/intents_exercise/MainActivity.java
@@ -1,34 +1,43 @@
 package com.example.intents_exercise;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText eText;
     EditText eTNumber;
+    SharedPreferences prefs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         eText = findViewById(R.id.write);
-        eTNumber=findViewById(R.id.writeNum);
-
+        eTNumber = findViewById(R.id.writeNum);
+        prefs = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
+        String result = eText.getText().toString();
+        result = prefs.getString("text","");
+        eText.setText(result);
     }
 
     public void buttonOpenPressed(View view) {
         Intent intent = new Intent(MainActivity.this, SecondActivity.class);
         String result = eText.getText().toString();
-        String resultNumText = eTNumber.getText().toString();
-        int resultNum = Integer.parseInt(resultNumText);
+        int resultNum = Integer.parseInt(eTNumber.getText().toString());
         intent.putExtra("text", result);
         intent.putExtra("number", resultNum);
         startActivity(intent);
     }
+
+
 
     public void buttonSharePressed(View view) {
         Intent intent = new Intent(Intent.ACTION_SEND);
@@ -59,7 +68,19 @@ public class MainActivity extends AppCompatActivity {
         String uri = String.format(txt);
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
         startActivity(intent);
+    }
 
+    public void buttonSavePressed(View view) {
+        SharedPreferences.Editor editor = prefs.edit();
+        String result = eText.getText().toString();
+        editor.putString("text", result);
+        editor.commit();
+        result = prefs.getString("text",result);
+        eText.setText(result);
+    }
+
+    public void buttonTimesPressed(View view){
+        Toast.makeText(this, (getString(R.string.timesCounter)+" "+prefs.getInt("counter",0)+" "+(getString(R.string.timesCounter2))), Toast.LENGTH_SHORT).show();
     }
 }
 
--- Intents_Exercise/app/src/main/java/com/example/intents_exercise/SecondActivity.java
+++ Intents_Exercise/app/src/main/java/com/example/intents_exercise/SecondActivity.java
@@ -1,22 +1,32 @@
 package com.example.intents_exercise;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
     TextView tvResult;
+    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         tvResult = findViewById(R.id.text);
-
+        prefs = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
         String text = getIntent().getStringExtra("text");
         int number = getIntent().getIntExtra("number",0);
 
         tvResult.setText(text+ " " + number);
+
+        int counter = prefs.getInt("counter",0);
+        counter++;
+
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("counter", counter);
+        editor.commit();
 
     }
 
